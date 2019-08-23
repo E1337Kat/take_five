c2eb292761e3b8e670373cad547ce7b224c0e2ca
Bruce Tate <bruce@rapidred.com>
2019-08-23 17:01:33 -0400

    fix compile error

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,7 @@ config :nerves_network,
   
 key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
 
-home = [
+network = [
           ssid: System.get_env("NERVES_NETWORK_SSID"),
           psk: System.get_env("NERVES_NETWORK_PSK"),
           key_mgmt: String.to_atom(key_mgmt),
